ebb973f30e002d9f6b27f7481339984a2f238379
AlexFairy <AlexM121687@gmail.com>
2024-08-16 17:20:39 -0700

    add initial project description

---
+++ ErrorsHW.py
@@ -0,0 +1,31 @@
+# 1. Exceptional Weather Forecast
+
+#Objective: 
+# The aim of this assignment is to enhance your understanding of exception handling by creating a weather forecast application that gracefully handles unexpected user input and provides user-friendly error messages.
+
+#Task 1: Start Begin by asking the user to enter the temperature in Fahrenheit.
+
+#Task 2: Temperature Conversion
+#Write a function that converts the Fahrenheit temperature to Celsius. 
+# Remember that the formula is (Fahrenheit - 32) * 5/9.
+#Use a try block to catch any potential errors during the conversion process. 
+# What happens if they type out "thirty" instead of doing 30?
+
+#Task 3: 
+# User Experience Implement an else block that prints the converted temperature in a user-friendly format. 
+#Example: "100 degrees Fahrenheit is 37.78 degrees Celsius."
+
+#Task 4: Finally Add a finally block that thanks the user for using the weather forecast application, 
+# ensuring that this message is displayed regardless of whether an exception was caught or not.
+
+try:
+  user_input = int(input("Please enter your temperature: "))
+  fahrenheit = (user_input - 32) * 5/9 
+
+  print(f"{user_input} degrees Fahrenheit is {fahrenheit} degrees Celsius")
+except ValueError :
+    print("Invalid Input. Please use numbers!")
+except Exception as e:
+   print("Other errors detected!")
+finally:
+   print("Thanks for using the weather app!")
